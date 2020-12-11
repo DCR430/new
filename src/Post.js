33ee76e48c09a9cf80a73b0876a670faa1cb276f
DCR430 <daniel.c.reyes30@gmail.com>
2020-12-10 20:41:43 -0500

    state

--- src/Post.js
+++ src/Post.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
 
 export default class Post extends Component {
+
+    state ={
+        value: ""
+    }
+
+    handleChange(event) {
+        this.setState({value: this.state.value});
+      }
+
+      handleSubmit(event) {
+        alert('A thought was submitted ' + this.state.value);
+        event.preventDefault();
+      }
+
+
     render() {
         return (
             <div>
-               <input type="email"
-               value="enter text">
+               <input type="text" value="enter text" value={this.state.value} onChange={this.handleChange}>
 
                </input>
                    <p>Please enter Thoughts</p>
